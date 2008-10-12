61d99276f937c493929a05a8569bd19519e20600
G. Allen Morris III <gam3@gam3.net>
2008-10-12 14:56:53 -0400

    Support code for setup widget.

---
+++ qt4/tlist/setup.cpp
@@ -0,0 +1,61 @@
+
+#include <iostream>
+
+#include <QCheckBox>
+#include "setup.h"
+
+Setup::Setup(QWidget * parent):QDialog (parent)
+{
+    setupUi(this);
+}
+
+Setup::Setup(const QString & host, const quint16 & port, const bool & ssl,
+	     const QString & user, const QString & password,
+	     QWidget * parent) : QDialog (parent)
+{
+    setupUi(this);
+    hostEdit->setText(host);
+    portEdit->setText(QString("%1").arg(port));
+    userEdit->setText(user);
+    if (password.size()) {
+	passwordEdit->setText(password);
+	remPass->setCheckState(Qt::Checked);
+    }
+    if (ssl)
+	useSSL->setCheckState(Qt::Checked);
+}
+
+#include <QSettings>
+
+void Setup::saveData()
+{
+    QString host = hostEdit->text();
+    QString user = userEdit->text();
+    QString password = passwordEdit->text();
+    qint16 port = portEdit->text().toInt();
+    bool ssl = useSSL->isChecked();
+
+    QSettings settings("Tasker", "tlist");
+
+    settings.beginGroup("Host");
+    settings.setValue("host", host);
+    settings.setValue("port", port);
+    settings.setValue("ssl", ssl);
+    settings.endGroup();
+
+    settings.beginGroup("User");
+    settings.setValue("user", user);
+    if (remPass->isChecked())
+	settings.setValue("password", password);
+    settings.endGroup();
+
+    portEdit->setText(QString("%1").arg(port));
+    userEdit->setText(user);
+    passwordEdit->setText(password);
+
+    emit setup(host, port, ssl, user, password);
+std::cerr << "Setup::saveData()" << std::endl;
+    done(0);
+}
+
+/* eof */
---
+++ qt4/tlist/setup.h
@@ -0,0 +1,24 @@
+
+
+#ifndef SETUP_H
+#define SETUP_H
+
+#include <QDialog>
+
+#include "ui_setup.h"
+
+class Setup : public QDialog, private Ui::SetupHost {
+    Q_OBJECT
+
+public:
+    Setup(QWidget *parent = 0);
+    Setup(const QString & host, const quint16 &port, const bool &ssl, const QString & user, const QString & password, QWidget * parent = 0);
+
+private slots:
+    void saveData();
+
+signals:
+    void setup(QString host, quint16 port, bool ssl, QString user, QString password);
+};
+
+#endif
